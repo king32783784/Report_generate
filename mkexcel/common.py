f060ebc63b1a18b99a1b39a39579be2cb8525d61
peng.li <peng.li@i-soft.com.cn>
2016-07-06 17:37:27 +0800

    modify common.py

--- mkexcel/common.py
+++ mkexcel/common.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+#usr/bin/env python
 # coding: utf-8
 #######################################################################
 #
@@ -13,41 +13,68 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 #xls_dirt字典保存基础信息
-sheet_cpu = {
+sheet_cpu_info = {
     'sheetname':'处理器运算',
-    'testinfo' : ("处理器运算性能", "测试工具：SPEC CPU 2000",\
-        "性能指标： Spec2000 包括 SPECint2000、SPECfp2000、SPECint_rate2000、 SPECfp_rate2000 4个测试项",
+    'testinfo' : ("处理器运算性能", "测试工具：SPEC CPU 2000",
+        "性能指标： Spec2000 包括 SPECint2000、SPECfp2000、\
+         SPECint_rate2000、 SPECfp_rate2000 4个测试项",
         "对比说明：其中的得分越大说明CPU性能越高", 
-        "测试参数： runspec -c test.cfg -i ref -n 3 -r -u 4 -I all; runspec -c test.cfg -i ref -n 3 -I all",
-                   "SPEC2000 测试数据"),
-    'basedata' : [
-        ["ITEM", "SPECint2000", "SPECfp2000", "SPECint_rate2000", "SPECfp_rate2000"],
-        ["isoft_desktop V4.0 loongson", "Deepin15_mips_20160520", "Neokylin Desktop-7.0-loongson"],
+        "测试参数： runspec -c test.cfg -i ref -n 3 -r -u 4 -I all;\
+        runspec -c test.cfg -i ref -n 3 -I all",
+        ),
+    "oslist": ["isoft_desktop V4.0 loongson", "Deepin15_mips_20160520", "Neokylin Desktop-7.0-loongson"],
+    }
+sheet_cpu_data = [
+    [
+        ["spec2000， bigger is better", "spec2000"],
+        ["ITEM", "SPECint2000", "SPECfp2000", "SPECint_rate2000",\
+        "SPECfp_rate2000"],
         [611.00, 775.00, 776.00, 777.00],
         [608.00, 776.00, 777.00, 778.00],
         [610.00, 777.00, 778.00, 779.00],
         [120.00, 124.00, 492.00, 340.00],
         [120.00, 124.00, 492.00, 340.00],
         ],
-    'layout' : [
-    ['A1:E1', 'A2:E2', 'A3:E3', 'A4:E4', 'A5:E5', 'A7:E7', 'A8', 'A9'],
-    ['45', '21', '24']],
-    'chart' : (730, 320, "Spec2000"),
-}
+]
+
 sheet_lmbench_info = {
-    'sheetname': '处理器运算',
-    'testinfo': ("测试工具：lmbench", "性能指标：选取了Processor、Context switching、*Local* Communication latencies 、File & VM system latencies 、*Local* Communication bandwidths等指标", "对比说明：测试结果均为测试3次求平均值"),
+    'sheetname': '内核',
+    'testinfo': ("内核性能测试","测试工具：lmbench", 
+        "性能指标：选取了Processor、Context switching、*Local* Communication\
+         latencies 、File & VM system latencies 、\n    *Local* Communication\
+         bandwidths等指标", "对比说明：测试结果均为测试3次求平均值",
+         "测试参数：以root用户执行测试，运行make result，之后继续运行两次\
+         make rerun，最后执行make see"),
     'oslist': ["isoft_desktop V4.0 loongson", "Deepin15_mips_20160520", "Neokylin Desktop-7.0-loongson"],
     }
-sheet_lmbench_data1 = {
-    'basedata': [
-        ["ITEM", "null call", "null I/O", "stat", "open clos", "slct TCP", "sig inst", "sig hndl", "fork proc(k)", "exec porc(k)", "sh proc(k)"],
+
+sheet_lmbench_data =[
+    [
+        ["Processor, Processes - times in microseconds - smaller is better", \
+        "Processor"],
+        ["ITEM", "null call", "null I/O", "stat", "open clos", "slct TCP",\
+         "sig inst", "sig hndl", "fork proc(k)", "exec porc(k)", "sh proc(k)"],
         [0.44, 0.70, 8.18, 12.26, 12.38, 0.80, 4.06, 0.42, 1.36, 4.46],
         [0.33, 0.67, 3.74, 7.24, 12.64, 1.04, 4.27, 0.39, 1.31, 5.24],
         [0.44, 0.68, 8.14, 13.10, 9.97, 0.82, 4.50, 0.43, 1.38, 4.29],
-    ]
-    }
-       
+    ],
+    [
+        ["Context switching - times in microseconds - smaller is better",\
+        "Context switching"],
+        ["ITEM", "2p/0k", "2p/16k", "2p/64k", "8p/16k", "8p/64k", "16p/16k",\
+        "16p/64k"],
+        [2.62, 3.04, 6.76, 5.44, 7.64, 6.13, 7.03],
+        [2.38, 2.51, 11.72, 6.80, 12.08, 8.00, 12.10],
+        [2.90, 2.96, 7.59, 6.00, 8.66, 6.98, 8.63],
+    ],
+    [
+        ["*Local* Communication latencies in microseconds - smaller is better", "*Local* Communication latencie"],
+        ["ITEM", "2p/0K ctxsw", "Pipe", "AF UNIX", "UDP", "TCP", "TCP conn"],
+        [2.62, 11.70, 21.26, 27.20, 36.44, 85.00],
+        [2.38, 11.46, 20.24, 24.82, 33.98, 115.80],
+        [2.90, 12.80, 23.34, 47.72, 61.72, 174.80],
+    ],
+]
     
 #format_dirt字典保存格式化信息
 #结果图表title格式
@@ -59,7 +86,6 @@ formtitle_format = {
     'align': 'center', #设置对齐方式
     'valign': 'vcenter', #设置对齐方式
 }
-
 #页title格式
 sheettitle_format = {
     'bold':     True,
@@ -112,74 +138,74 @@ item_format = {
     'fg_color': '#CCFFFF',
 }
 
-
-class mk_singleitem(object):
-    def __init__(self, xls_dirt, xlsname):
-        self.dirt = xls_dirt
+# 测试结果分析title格式
+resulttitle_format = {
+    'bold': True,
+    'border': 1,
+    'font_size': 12,
+    'align': 'center',
+    'valign': 'vcenter',
+    'fg_color': '#339966',
+}
+class MkSinglesheet(object):
+    def __init__(self, iteminfo, itemdata, xlsname):
+        self.info = iteminfo
+        self.data = itemdata
         self.xlsname = xlsname
+        self.layoutinfo = self.layoutinformation()
         self.workbook = self.mkxls()
 
     def mkxls(self):
         workbook = xlsxwriter.Workbook(self.xlsname)
         return workbook
 
-    def addsheet(self):
-        worksheet = self.workbook.add_worksheet(self.dirt['sheetname'])
-        self.input_sheettitle(worksheet)
-        self.input_testinfo(worksheet)
-        self.input_formsubtitle(worksheet)
-        self.input_formtitle(worksheet)
-        self.input_formitem(worksheet)
-        self.input_formdata(worksheet)
-        self.setcellformat(worksheet)
-        self.mk_chart(worksheet)
-        self.workbookclose()
-
-    def numbersystems(self):
-        itemsnum = len(self.dirt['basedata'][0])
-        seriesnum = len(self.dirt['basedata'][1])
-        formnum = (itemsnum, seriesnum)
+    def layoutinformation(self):
+        formnum = {}
+        formnum['seriesnum'] = len(self.info['oslist'])
+        columnwidth = []
+        for item in self.data:
+            columnwidth.append(len(item[1])-1)
+        columnno = []
+        for i in columnwidth:
+            columnno.append(string.uppercase[i])
+        formnum['columnwidth'] = columnno
         return formnum
 
-    def input_sheettitle(self, worksheet):
+    def input_sheettitle(self, worksheet, formatinfo):
         format = self.workbook.add_format(sheettitle_format)
-        worksheet.merge_range(self.dirt['layout'][0][0], self.dirt['testinfo'][0], format)
+        worksheet.merge_range('A1:%s1' % formatinfo['largerow'],\
+                              self.info['testinfo'][0], format)
         
-    def input_testinfo(self, worksheet):
+    def input_testinfo(self, worksheet, formatinfo):
         format = self.workbook.add_format(info_fortmat)
-        worksheet.merge_range(self.dirt['layout'][0][1], self.dirt['testinfo'][1], format)
-        worksheet.merge_range(self.dirt['layout'][0][2], self.dirt['testinfo'][2], format)
-        worksheet.merge_range(self.dirt['layout'][0][3], self.dirt['testinfo'][3], format)
-        worksheet.merge_range(self.dirt['layout'][0][4], self.dirt['testinfo'][4], format)
+        worksheet.merge_range('A2:%s2' % formatinfo['largerow'],\
+                              self.info['testinfo'][1], format)
+        worksheet.merge_range('A3:%s3' % formatinfo['largerow'],\
+                              self.info['testinfo'][2], format)
+        worksheet.merge_range('A4:%s4' % formatinfo['largerow'],\
+                              self.info['testinfo'][3], format)
+        worksheet.merge_range('A5:%s5' % formatinfo['largerow'],\
+                              self.info['testinfo'][4], format)
    
-    def input_formsubtitle(self, worksheet):
+    def input_formsubtitle(self, worksheet, linenum, columnnum, iteminfo):
         format = self.workbook.add_format(formsubtitle_format)
-        worksheet.merge_range(self.dirt['layout'][0][5], self.dirt['testinfo'][5], format)
+        worksheet.merge_range('A%s:%s%s' %(linenum, columnnum, linenum),iteminfo, format)
 
-    def input_formtitle(self, worksheet):
+    def input_formtitle(self, worksheet, linenum, iteminfo):
         format = self.workbook.add_format(formtitle_format)
-        worksheet.write_row(self.dirt['layout'][0][6], self.dirt['basedata'][0], format)
+        worksheet.write_row('A%s' % linenum, iteminfo, format)
 
-    def input_formitem(self, worksheet):
+    def input_formitem(self, worksheet, linenum, seriesinfo):
         format = self.workbook.add_format(item_format)
-        worksheet.write_column(self.dirt['layout'][0][7], self.dirt['basedata'][1], format)
+        worksheet.write_column('A%s' % linenum, seriesinfo, format)
 
-    def input_formdata(self, worksheet):
+    def input_formdata(self, worksheet, linenum, testdata):
         format = self.workbook.add_format(result_format)
-        formnum = self.numbersystems()
-        lastline = 9 + formnum[1]
-        for data, line in zip(self.dirt['basedata'][2:], range(9,lastline)):
+        lastline = linenum + self.layoutinfo['seriesnum']
+        for data, line in zip(testdata[2:], range(linenum,lastline)):
             worksheet.write_row('B%s' % line, data, format)
- 
-    def setbandwith(self, data):
-        length = 0
-        for charter in data:
-            if len(charter) > length:
-                length = len(charter)
-        return length
 
     def setcellformat(self, worksheet):
-        formnum = self.numbersystems()
         firstrow = float(self.setbandwith(self.dirt['basedata'][1]) / 1.2)
         otherrow = float(self.setbandwith(self.dirt['basedata'][0]) * 1.2)
         rows = formnum[0]
@@ -187,48 +213,124 @@ class mk_singleitem(object):
         lastline = 8 + lines
         worksheet.set_column(0, 0, firstrow)
         worksheet.set_column(1, rows+1, otherrow)
-        worksheet.set_row(0, float(self.dirt['layout'][1][0]))
-        for i in range(1, 5):
-            worksheet.set_row(i, float(self.dirt['layout'][1][1]))
-        for i in range(7, lastline):
-            worksheet.set_row(i, float(self.dirt['layout'][1][2]))
-    
 
-    def mk_chart(self, worksheet):
+    def setbandwidth(self, data):
+        length = 0
+        for charter in data:
+            if len(charter) > length:
+                length = len(charter)
+        return length
+
+    def formatinfo(self):
+        formatinfodict = {}
+        columnwidthst = float(self.setbandwidth(self.info['oslist']) / 1.2)
+        formatinfodict['columnwidthst'] = columnwidthst
+        columnwidthother = 0
+        rows = 0
+        for i, itemname in enumerate(self.data):
+            if self.setbandwidth(self.data[i][1]) > columnwidthother:
+                columnwidthother = self.setbandwidth(self.data[i][1])
+            if len(self.data[i][1]) > rows:
+                rows = len(self.data[i][1])
+        columnwidthother =float(columnwidthother * 1.2) 
+        formatinfodict['columnwidthother'] = columnwidthother
+        formatinfodict['rowslarge'] = rows
+        formatinfodict['largerow'] = string.uppercase[rows-1]
+        figwidelist = []
+        for i, itemname in enumerate(self.data):
+            figwidelist.append((columnwidthst + columnwidthother * len(self.data[i][1])) * 5.5)
+        formatinfodict['figwidelist'] = figwidelist
+        print formatinfodict
+        return formatinfodict
+
+    def sheetformatting(self, worksheet, formatinfo):
+        worksheet.set_row(0, 45)
+        startline = 7
+        lastline = startline + 2 + self.layoutinfo['seriesnum']
+        step = 20 + self.layoutinfo['seriesnum']
+        for i, item in enumerate(self.data):
+            for j in range(startline, lastline):
+                worksheet.set_row(j, 19)
+            startline = startline + step
+            lastline = lastline + step
+        for i in range(1, 5):
+            worksheet.set_row(i, 21)
+        worksheet.set_column(0, 0, formatinfo['columnwidthst'])
+        worksheet.set_column(1, formatinfo['rowslarge'], formatinfo['columnwidthother'])
+        
+
+    def mk_chart(self, worksheet, chartwidth, linenum, figwide, itemdata):
+        item_linenum = linenum + 1
+        series_linenum = item_linenum + 1
         chart = self.workbook.add_chart({'type': 'column'})     
-        formnum =self.numbersystems()
-        seriesnum = formnum[1]
-        itemsnum = formnum[0]
-        columnnumber = string.uppercase[itemsnum] #E
-        print formnum
-        if seriesnum < 1:
-            print(" There is no data ")
-            exit()
+        seriesnum = self.layoutinfo['seriesnum']
         chart.add_series({
-            'name' : '=%s!$A$9' % (worksheet.name) ,#系列名称
-            'categories': '=%s!$B$8:$%s$8' % (worksheet.name, columnnumber), #对比项目名称
-            'values': '=%s!$B$9:$%s$9' % (worksheet.name, columnnumber),
+            'name' : '=%s!$A$%s' % (worksheet.name, series_linenum) ,#系列名称
+            'categories': '=%s!$B$%s:$%s$%s' % (worksheet.name, item_linenum,\
+                                                chartwidth, item_linenum),
+            'values': '=%s!$B$%s:$%s$%s' % (worksheet.name, series_linenum,\
+                                            chartwidth, series_linenum),
             'gap': 200,
             })
-        for i, osname in enumerate(self.dirt['basedata'][1][1:]):
-            chart.add_series({
-                'name' : '=%s!$A$%s' % (worksheet.name, i+10),
-                'values': '=%s!$B$%s:$%s$%s' % (worksheet.name, i+10, columnnumber, i+10)
-            })
-        chart_width = self.dirt['chart'][0]
-        chart_height = self.dirt['chart'][1]
-        chart.set_size({'width': chart_width, 'height': chart_height})
+        if seriesnum > 1:
+            for i, osname in enumerate(self.info['oslist'][0:-1]):
+                lineno = i + series_linenum + 1
+                chart.add_series({
+                    'name' : '=%s!$A$%s' % (worksheet.name, lineno),
+                    'values': '=%s!$B$%s:$%s$%s' % (worksheet.name, lineno, \
+                                                    chartwidth, lineno)
+                })
+        chart.set_size({'width': figwide, 'height': 320})
         chart.set_x_axis({
             'position_axis': 'between',
         })
-        chart_titlename = self.dirt['chart'][2]
+        chart_titlename = itemdata[0][1]
         chart.set_title({'name': chart_titlename})
-        chart_insert_site = 'A%s' % int(10+seriesnum)
+        chart_insert_site = 'A%s' % int(series_linenum + 1 + seriesnum)
         worksheet.insert_chart(chart_insert_site, chart)
+   
+    def additems(self, worksheet, formatinfo):
+        titleline = 7
+        itemline = 8
+        seriesline = 9
+        step = 20 + self.layoutinfo['seriesnum']
+        for i, additem in enumerate(self.data):
+            self.input_formsubtitle(worksheet, titleline, self.layoutinfo['columnwidth'][i], self.data[i][0][0])
+            self.input_formtitle(worksheet, itemline, self.data[i][1])
+            self.input_formitem(worksheet, seriesline, self.info['oslist'])
+            self.input_formdata(worksheet, seriesline, self.data[i])
+            self.mk_chart(worksheet, self.layoutinfo['columnwidth'][i], titleline, formatinfo['figwidelist'][i], self.data[i])
+            titleline = titleline + step
+            itemline = itemline + step
+            seriesline = seriesline + step
+        self.addresultanalysistitle(worksheet, titleline, formatinfo['largerow']) 
+        self.addresultanalysis(worksheet, titleline+1, formatinfo['largerow'])
+
+    def addresultanalysistitle(self, worksheet, lastline, columnwidth):
+        format = self.workbook.add_format(resulttitle_format)
+        worksheet.merge_range('A%s:%s%s' %(lastline, columnwidth, lastline), "测试结果分析", format)
+
+    def addresultanalysis(self, worksheet, lastline, columnwidth):
+        format = self.workbook.add_format(item_format)
+        worksheet.merge_range('A%s:%s%s' %(lastline, columnwidth, lastline+3), " ", format)
+
+
+    def addsheet(self):
+        formatinfo = self.formatinfo()
+        worksheet = self.workbook.add_worksheet(self.info['sheetname'])
+        self.input_sheettitle(worksheet, formatinfo)
+        self.input_testinfo(worksheet, formatinfo)
+        self.additems(worksheet, formatinfo)
+        self.sheetformatting(worksheet, formatinfo)
+        self.workbookclose()
 
     def workbookclose(self):
         self.workbook.close()
 
 
-a = mk_singleitem(sheet_cpu, 'test.xlsx')
+class Mk_xls(object):
+
+a = MkSinglesheet(sheet_cpu_info, sheet_cpu_data, 'test.xlsx')
+a.addsheet()
+a = MkSinglesheet(sheet_lmbench_info, sheet_lmbench_data, 'test.xlsx')
 a.addsheet()
